43406b24a49eeaf07f64cbef83b1b6256b0ef40d
asathis2 <asathis2@illinois.edu>
2018-05-10 18:46:04 -0500

    splunk tests

--- test_1.py
+++ test_1.py
@@ -1,10 +1,7 @@
-# test_capitalize.py
 import requests
 import pytest
 import json
 
-
-# test_capitalize.py
 
 @pytest.fixture
 def get_req():
@@ -17,6 +14,7 @@ def get_req():
 def test_get_req(get_req):
     contents = get_req
     assert (contents[0].status_code) == 200
+
 
 def test_same_image(get_req):
     d = get_req[1]
@@ -33,11 +31,58 @@ def test_valid_poster_link(get_req):
     d = get_req[1]
     results = ((d['results']))
     for movies in results:
-        if 'poster_path' not in movies:
-            continue
         if (movies['poster_path']) is None:
             continue
         ret = requests.head(movies['poster_path'])
-        assert ret.status_code < 400
+        assert ret.status_code < 400  # invalid link
 
 
+def test_sorting_req(get_req):
+    d = get_req[1]
+    results = ((d['results']))
+    Genre_none_sort = True
+    prev_id = -1
+    for movies in results:
+        assert (movies['id']) > prev_id  # not in ascending order
+        prev_id = (movies['id'])
+        if movies['genre_ids'] is None:
+            assert Genre_none_sort is True  # Null genre ids after non nulls
+            continue
+        else:
+            Genre_none_sort = False
+
+
+def test_genreid_sum(get_req):
+    d = get_req[1]
+    results = ((d['results']))
+    count_gid_400 = 0
+    for movies in results:
+        if movies['genre_ids'] is None:
+            continue
+        else:
+            if sum(movies['genre_ids']) > 400:
+                count_gid_400 += 1
+        assert count_gid_400 < 8
+
+
+def test_palind_title(get_req):
+    d = get_req[1]
+    results = ((d['results']))
+    count_pals = 0
+    pal_titles=[]
+    for movies in results:
+        if movies['title'] is None:
+            continue
+        else:
+            title = movies['title']
+            words_title = title.split()
+            for word in words_title:
+                len_t = len(word)
+                pal = True
+                for c in range(int(len_t / 2)):
+                    if word[c] != word[len_t - 1 - c]:
+                        pal = False
+                if pal == True:
+                    pal_titles.append(title)
+                    count_pals += 1
+    assert count_pals > 0
